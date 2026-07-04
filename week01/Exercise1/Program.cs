abaaa1f36afc525af9967cd4421f5249b09bfcde
Albert Esteves <albertesteves2019@gmail.com>
2026-07-03 20:40:05 -0700

    Exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,9 @@
-using System;
+Console.Write("What is your first name? ");
+string name = Console.ReadLine();
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
-    }
-}
+Console.Write("What is your last name? ");
+string lastname = Console.ReadLine();
+
+Console.Write($"Your name is {lastname}, {name} {lastname}");
+
+
