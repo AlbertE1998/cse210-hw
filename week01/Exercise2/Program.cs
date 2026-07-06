d69514b49ab46c5b44a4bd243a10dd9fc2bfbcb1
Albert Esteves <albertesteves2019@gmail.com>
2026-07-05 17:22:02 -0700

    Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,13 +1,38 @@
-Console.Write("What is your grade percentage?");
+Console.Write("What is your grade percentage? ");
 string gradeper = Console.ReadLine();
 int percentage = int.Parse(gradeper);
 
+string letter = "";
+
 if (percentage >= 90)
 {
-    Console.Write("A");
+    letter = "A";
 }
 
-else if (percentage >= 80 && percentage < 89)
+else if (percentage >= 80 && percentage < 90)
 {
-    Console.Write("B");
+    letter = "B";
 }
+
+else if (percentage >= 70 && percentage < 80)
+{
+    letter = "C";
+}
+
+else if (percentage >= 60 && percentage < 70)
+{
+    letter = "D";
+}
+
+else
+{
+    letter = "F";
+}
+
+Console.WriteLine($"Your grade is {letter}");
+
+if(percentage >= 70)
+Console.Write("Congratulations, you passed!");
+
+else
+Console.Write("You failed this time, I know you will succeed next!");
