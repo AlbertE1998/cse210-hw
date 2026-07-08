2349eb719c95221cf320044d3f1e24a2914d886a
Albert Esteves <albertesteves2019@gmail.com>
2026-07-07 21:08:57 -0700

    Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,38 +1,48 @@
-Console.Write("What is your grade percentage? ");
-string gradeper = Console.ReadLine();
-int percentage = int.Parse(gradeper);
+using System;
 
-string letter = "";
-
-if (percentage >= 90)
+class Program
 {
-    letter = "A";
+    static void Main(string[] args)
+    {
+
+    Console.Write("What is your grade percentage? ");
+    string gradeper = Console.ReadLine();
+    int percentage = int.Parse(gradeper);
+
+    string letter = "";
+
+    if (percentage >= 90)
+    {
+        letter = "A";
+    }
+
+    else if (percentage >= 80 && percentage < 90)
+    {
+        letter = "B";
+    }
+
+    else if (percentage >= 70 && percentage < 80)
+    {
+        letter = "C";
+    }
+
+    else if (percentage >= 60 && percentage < 70)
+    {
+        letter = "D";
+    }
+
+    else
+    {
+        letter = "F";
+    }
+
+    Console.WriteLine($"Your grade is {letter}");
+
+    if(percentage >= 70)
+    Console.Write("Congratulations, you passed!");
+
+    else
+    Console.Write("You failed this time, I know you will succeed next!");
+    }
+
 }
-
-else if (percentage >= 80 && percentage < 90)
-{
-    letter = "B";
-}
-
-else if (percentage >= 70 && percentage < 80)
-{
-    letter = "C";
-}
-
-else if (percentage >= 60 && percentage < 70)
-{
-    letter = "D";
-}
-
-else
-{
-    letter = "F";
-}
-
-Console.WriteLine($"Your grade is {letter}");
-
-if(percentage >= 70)
-Console.Write("Congratulations, you passed!");
-
-else
-Console.Write("You failed this time, I know you will succeed next!");
