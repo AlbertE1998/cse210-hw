b9a5284fb80c8d3006a1fea689907d22d73d02f8
AlbertE1998 <albertesteves2019@gmail.com>
2026-07-22 03:57:58 +0000

    Scripture memorizer project

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -3,13 +3,29 @@ using System;
 class Program
 {
     static void Main(string[] args)
+{
+    List<Scripture> scriptures = new List<Scripture>()
     {
-        Reference reference = new Reference("Proverbs", 3, 5, 6);
-
-        Scripture scripture = new Scripture(
-            reference,
+        new Scripture(
+            new Reference("Proverbs", 3, 5, 6),
             "Trust in the Lord with all thine heart and lean not unto thine own understanding."
-        );
+        ),
+
+        new Scripture(
+            new Reference("John", 3, 16),
+            "For God so loved the world, that he gave his only begotten Son."
+        ),
+
+        new Scripture(
+            new Reference("Mosiah", 2, 17),
+            "When ye are in the service of your fellow beings ye are only in the service of your God."
+        )
+    };
+
+    Random random = new Random();
+
+    Scripture scripture = scriptures[random.Next(scriptures.Count)];
+
 
         while (!scripture.IsCompletelyHidden())
         {
